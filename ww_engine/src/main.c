6c532ee39aa00fb8093e5de8220a6cb0e869923d
Wei Liu <liuweiky@qq.com>
2014-02-19 18:15:48 +0800

    1

--- ww_engine/src/main.c
+++ ww_engine/src/main.c
@@ -7,19 +7,19 @@
 #include <stdio.h>
 #include "nids.h"
 
+#define DEBUG(a...) fprintf(stderr, ##a);
+
 #define int_ntoa(x)	inet_ntoa(*((struct in_addr *)&x))
 int content_flag = 0;
 
-// struct tuple4 contains addresses and port numbers of the TCP connections
-// the following auxiliary function produces a string looking like
 // 10.0.0.1,1024,10.0.0.2,23
 char * adres (struct tuple4 addr)
 {
     static char buf[256];
     strcpy (buf, int_ntoa (addr.saddr));
-    sprintf (buf + strlen (buf), ",%i,", addr.source);
+    sprintf (buf + strlen (buf), ":%i,", addr.source);
     strcat (buf, int_ntoa (addr.daddr));
-    sprintf (buf + strlen (buf), ",%i", addr.dest);
+    sprintf (buf + strlen (buf), ":%i", addr.dest);
     return buf;
 }
 
@@ -30,42 +30,26 @@ void tcp_callback (struct tcp_stream *a_tcp, void ** this_time_not_needed)
     strcpy (buf + strlen(buf), adres (a_tcp->addr)); // we put conn params into buf
     if (a_tcp->nids_state == NIDS_JUST_EST)
     {
-        // connection described by a_tcp is established
-        // here we decide, if we wish to follow this stream
-        // sample condition: if (a_tcp->addr.dest!=23) return;
-        // in this simple app we follow each stream, so..
-        a_tcp->client.collect++; // we want data received by a client
-        a_tcp->server.collect++; // and by a server, too
-        a_tcp->server.collect_urg++; // we want urgent data received by a
-        // server
-#ifdef WE_WANT_URGENT_DATA_RECEIVED_BY_A_CLIENT
-        a_tcp->client.collect_urg++; // if we don't increase this value,
-        // we won't be notified of urgent data
-        // arrival
-#endif
-        fprintf (stderr, "%s 建立连接\n", buf);
+        a_tcp->client.collect++;
+        a_tcp->server.collect++;
+        a_tcp->server.collect_urg++;
+        DEBUG("%s 建立连接\n", buf);
         return;
     }
     if (a_tcp->nids_state == NIDS_CLOSE)
     {
-        // connection has been closed normally
-        fprintf (stderr, "%s 关闭连接\n", buf);
+        DEBUG("%s 关闭连接\n", buf);
         return;
     }
     if (a_tcp->nids_state == NIDS_RESET)
     {
-        // connection has been closed by RST
-        fprintf (stderr, "%s 重置\n", buf);
+        DEBUG("%s RESET\n", buf);
         return;
     }
 
     if (a_tcp->nids_state == NIDS_DATA)
     {
-        // new data has arrived; gotta determine in what direction
-        // and if it's urgent or not
-
         struct half_stream *hlf;
-
         if (a_tcp->server.count_new_urg)
         {
             // new byte of urgent data has arrived 
@@ -75,49 +59,38 @@ void tcp_callback (struct tcp_stream *a_tcp, void ** this_time_not_needed)
             write(1,buf,strlen(buf));
             return;
         }
-        // We don't have to check if urgent data to client has arrived,
-        // because we haven't increased a_tcp->client.collect_urg variable.
-        // So, we have some normal data to take care of.
         if (a_tcp->client.count_new)
         {
-            // new data for client
-            hlf = &a_tcp->client; // from now on, we will deal with hlf var,
-            // which will point to client side of conn
-            strcat (buf, "(<-)"); // symbolic direction of data
-        }
-        else
-        {
+            hlf = &a_tcp->client;
+            strcat (buf, "(<-)");
+        } else {
             hlf = &a_tcp->server; // analogical
             strcat (buf, "(->)");
         }
-        fprintf(stderr,"%s \n",buf); // we print the connection parameters
-        // (saddr, daddr, sport, dport) accompanied
-        // by data flow direction (-> or <-)
-
+        fprintf(stderr,"%s \n",buf);
         if (content_flag)
             write(2,hlf->data,hlf->count_new); // we print the newly arrived data
     }
     return ;
 }
 
-void udp_callback(struct tuple4 * addr, char *buf,  int len, struct ip *iph)
-{
-    int i = 0;
-    char buffer[1024];
-    sprintf(buffer, "=UDP=\t");
-    strcpy(buffer + strlen(buffer), inet_ntoa(*((struct in_addr *) &(addr->saddr))));
-    sprintf(buffer + strlen(buffer), ": %i ", addr->source);
-    strcat(buffer, " -> ");
-    strcpy(buffer + strlen(buffer), inet_ntoa(*((struct in_addr *) &(addr->daddr))));
-    sprintf(buffer + strlen(buffer), ": %i ", addr->dest);
-    fprintf(stderr,"%s \n",buffer);
-}
+//void udp_callback(struct tuple4 * addr, char *buf,  int len, struct ip *iph)
+//{
+//    int i = 0;
+//    char buffer[1024];
+//    sprintf(buffer, "=UDP=\t");
+//    strcpy(buffer + strlen(buffer), inet_ntoa(*((struct in_addr *) &(addr->saddr))));
+//    sprintf(buffer + strlen(buffer), ": %i ", addr->source);
+//    strcat(buffer, " -> ");
+//    strcpy(buffer + strlen(buffer), inet_ntoa(*((struct in_addr *) &(addr->daddr))));
+//    sprintf(buffer + strlen(buffer), ": %i ", addr->dest);
+//    fprintf(stderr,"%s \n",buffer);
+//}
 
 int main (int argc, char* argv[])
 {
-    if ( argc > 1) {
+    if (argc > 1)
         content_flag = 1;
-    }
 
     // here we can alter libnids params, for instance:
     // nids_params.n_hosts=256;
@@ -125,11 +98,11 @@ int main (int argc, char* argv[])
     if (!nids_init ())
     {
         fprintf(stderr,"%s\n",nids_errbuf);
-        exit(1);
+        return 1;
     }
 
     nids_register_tcp (tcp_callback);
-    nids_register_udp (udp_callback);
+    //nids_register_udp (udp_callback);
     nids_run ();
     return 0;
 }
